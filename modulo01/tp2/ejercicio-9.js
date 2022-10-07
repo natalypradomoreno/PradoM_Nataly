fee5d897c855d983d5776123e1e3936fd55f33fc
Nataly Prado <113069046+natalypradoo@users.noreply.github.com>
2022-10-07 09:52:52 -0300

    ejercicio 9 tp2 mod1

---
+++ modulo01/tp2/ejercicio-9.js
@@ -0,0 +1,10 @@
+var n1=prompt("Ingrese nota 1"); var n2=prompt("Ingrese nota 2");
+var n3=prompt ("Ingrese nota 3"); resultado=0
+if(n1,n2,n3< 11){
+resultado= (parseFloat(n1)+parseFloat(n2)+parseFloat(n3))/3
+if(resultado<5){
+console.log("Reprobado");}
+if(resultado<8){
+console.log("Aprobado")}
+if(resultado>8){
+console.log("Sobresaliente")}}
