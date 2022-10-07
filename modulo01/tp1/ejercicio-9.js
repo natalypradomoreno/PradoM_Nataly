acd188e8d477e4ad2acb89170ccfcbea395bc9e7
Nataly Prado <113069046+natalypradoo@users.noreply.github.com>
2022-10-07 09:44:31 -0300

    ejercicio 9 tp1 mod1

---
+++ modulo01/tp1/ejercicio-9.js
@@ -0,0 +1,19 @@
+modulo${1}/tp${1}/ejercicio-${9}
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+var array= ["a", "e", "i", "o", "u"]
+document.write(array[0]);
+document.write("</br>");
+document.write(array[1]);
+document.write("</br>");
+document.write(array[2]);
+document.write("</br>");
+document.write(array[3]);
+document.write("</br>");
+document.write(array[4]);
+document.write("</br>");
+</script>
+</body>
+</html>
