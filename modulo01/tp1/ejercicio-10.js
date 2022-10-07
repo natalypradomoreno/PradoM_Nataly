4e3edc1189d09e013c9e7b64bd88f910b061e18f
Nataly Prado <113069046+natalypradoo@users.noreply.github.com>
2022-10-07 09:45:28 -0300

    ejercicio 10 tp1 mod1

---
+++ modulo01/tp1/ejercicio-10.js
@@ -0,0 +1,53 @@
+modulo${1}/tp${1}/ejercicio-${10}
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8"></head>
+<body>
+<script type="text/javascript">
+var num;
+num=prompt("Introducir un numero: ","0");
+num=parseInt(num)
+switch(num){
+case 1:
+document.write("Tu mes es Enero");
+break;
+case 2:
+document.write("Tu mes es Febrero");
+break;
+case 3:
+document.write("Tu mes es Marzo");
+break;
+case 4:
+document.write("Tu mes es Abril");
+break;
+case 5:
+document.write("Tu mes es Mayoo");
+break;
+case 6:
+document.write("Tu mes es Junio");
+break;
+case 7:
+document.write("Tu mes es Julio");
+break;
+case 8:
+document.write("Tu mes es Agosto");
+break;
+case 9:
+document.write("Tu mes es Septiembre");
+break;
+case 10:
+document.write("Tu mes es Octubre");
+break;
+case 11:
+document.write("Tu mes es Noviembre");
+break;
+case 12:
+document.write("Tu mes es Diciembre");
+break;
+default:
+document.write("No existe un mes con ese número. Vuelva a
+ingresar nuevamente")
+}
+</script>
+</body>
+</html>
